82deb9e30b5a3a9dedb3c31f13976d9c4e7514ca
Gman1906Sibs <77187437+Gman1906Sibs@users.noreply.github.com>
2023-09-10 23:31:18 +0200

    name text

--- src/cards/PictureCard.js
+++ src/cards/PictureCard.js
@@ -4,6 +4,11 @@ import Gman from "../images/gman.jpg"
 function PictureCard() {
   return (
     <div className=' card w-[700px] h-[467px] flex justify-center items-center  rounded-[10px] overflow-hidden relative'>
+    <img src={Gman} alt="" className=' absolute z-30 w-[689px] contain rounded-[10px] ' />
+    <div className=" glass flex-col bottom-[5px] absolute z-40 w-[689px] contain rounded-[10px]">
+    <h3 className=' text-[30px] font-bold '>Gladman Sibanda</h3>
+    <p>Software Developer</p>
+    </div>
     </div>
   )
 }
