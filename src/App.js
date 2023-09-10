27bebe145bf40777eb8928dd90dd9abf23e3f464
Gman1906Sibs <77187437+Gman1906Sibs@users.noreply.github.com>
2023-09-10 23:15:48 +0200

    effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './Cards';
 
 function App() { 
   return (
     <div className="App">
-      <div className=" text-red-500 ">Hello World!...</div>
+      <Cards/>
     </div>
   );
 }
---
+++ src/cards/PictureCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Gman from "../images/gman.jpg"
+
+function PictureCard() {
+  return (
+    <div className=' card w-[700px] h-[467px] flex justify-center items-center  rounded-[10px] overflow-hidden relative'>
+    </div>
+  )
+}
+
+export default PictureCard
